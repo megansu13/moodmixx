3d315bbff4242f65e7644787966900219f14a8a0
Megan <softballmegan.513@gmail.com>
2024-04-21 18:26:31 -0400

    removed depende

--- moodmixx/__init__.py
+++ moodmixx/__init__.py
@@ -1,26 +1,12 @@
 import flask
 from flask_cors import CORS, cross_origin
-from flask import send_from_directory
-
-from .views.spots import spots
-from flask_cors import CORS, cross_origin
 
 
-def create_app():
-    # app is a single object used by all the code modules in this package
-    app = flask.Flask(__name__, static_folder='moodmixx/build', static_url_path='')  # pylint: disable=invalid-name
-    # CORS(app, supports_credentials=True)
-    CORS(app)
+# app is a single object used by all the code modules in this package
+app = flask.Flask(__name__, static_folder='moodmixx/build', static_url_path='')  # pylint: disable=invalid-name
+CORS(app, supports_credentials=True)
 
 # Read settings from config module (insta485/config.py)
-    app.config.from_object('moodmixx.config')
-    app.register_blueprint(spots)
+app.config.from_object('moodmixx.config')
 
-    @app.route('/', defaults={'path': ''})
-    @app.route('/<path:path>')
-    @cross_origin()
-    def serve(path):
-        if path != "" and os.path.exists(app.static_folder + '/' + path):
-            return send_from_directory(app.static_folder, path)
-        else:
-            return send_from_directory(app.static_folder, 'index.html')
+import moodmixx.views
